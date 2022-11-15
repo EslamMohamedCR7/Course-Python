515fd5455e5b361f22ff412e87dd8353fd9dee50
EsraaAbdElslam <eslamklanton@gmail.com>
2022-11-15 17:56:18 +0200

    EX01_09

---
+++ AreaAndPerimeterOfARectangle.py
@@ -0,0 +1,8 @@
+print("Area = width * height")
+print("Area = 4.5 * 7.9 ")
+print("Area Of Rectangle) Is  " + str( 4.5 * 7.9 ) + ".")
+print(" ")
+print("Perimeter = 2 * ( width + height ) ")
+print("Perimeter = 2 * ( 4.5 * 7.9 ) ")
+print("Perimeter Of Rectangle) Is  " + str( 2 * ( 4.5 * 7.9 ) ) + ".")
+ 
