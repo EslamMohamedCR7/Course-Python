f936cc4045b72a233838d9e5d91fef53b01a485a
EsraaAbdElslam <eslamklanton@gmail.com>
2022-11-15 18:10:59 +0200

    EX01_17

---
+++ (TurtleDrawALine.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.showturtle()
+turtle.color("red")
+turtle.penup()
+turtle.goto(-39, 48)
+turtle.right(45)
+turtle.pendown()
+turtle.write("(-39, 48)")
+turtle.goto(50, -50)
+turtle.write("(50, -50)")
+turtle.penup()
+turtle.done()
