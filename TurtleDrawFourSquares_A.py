f9b59d61df0e215a75505594da5a50d0d496d27c
EsraaAbdElslam <eslamklanton@gmail.com>
2022-11-15 18:03:21 +0200

    EX01_12

---
+++ TurtleDrawFourSquares_A.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.showturtle()
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(200)
+
